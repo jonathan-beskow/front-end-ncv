24f53750d3a4bc2f6b2b75fe559dc246ec7b76dc
M373746 <j.beskow@sistran.com.br>
2025-01-07 15:04:05 -0300

    inclusão página inicial

--- src/app/components/app-component/app.component.ts
+++ src/app/components/app-component/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { SHARED_IMPORTS } from '../../shared-imports';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, SHARED_IMPORTS],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
